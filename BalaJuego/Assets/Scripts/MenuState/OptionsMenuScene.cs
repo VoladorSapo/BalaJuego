fa43a87cd8bdd1b45fe04e4a6920a84439b1a47f
Taleren <edunett33@gmail.com>
2025-02-02 04:58:41 +0100

    pussy

--- BalaJuego/Assets/Scripts/MenuState/OptionsMenuScene.cs
+++ BalaJuego/Assets/Scripts/MenuState/OptionsMenuScene.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class OptionsMenuState : AMenuState
 {
+    Animator[] animators;
     public OptionsMenuState(MenuController menu) : base(menu)
     {
     }
@@ -14,17 +15,32 @@ public class OptionsMenuState : AMenuState
 
     public override void HandleButton(string i)
     {
-     
+        switch (i)
+        {
+            case "exit":
+                menu.SetState(new MainMenuState(menu));
+                break;
+        }
     }
 
     public override void OnEnter()
     {
-        menu.MainStateParent.SetActive(true);
+        //menu.MainStateParent.SetActive(true);
+        menu.OptionStateParent.SetActive(true);
+        animators = menu.OptionStateParent.GetComponentsInChildren<Animator>();
+        foreach (Animator an in animators)
+        {
+            an.Play("moveIn");
+        }
     }
 
     public override void OnExit()
     {
-        menu.MainStateParent.SetActive(false);
+        //menu.MainStateParent.SetActive(false);
+        foreach (Animator an in animators)
+        {
+            an.Play("moveOut");
+        }
     }
 
     public override void Update()
