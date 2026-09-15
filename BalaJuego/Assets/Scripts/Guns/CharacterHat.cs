218d891602d34868d6d8fd8f7004c1f8458b5924
Eduardo Almarza Blasco <122693648+Taleren@users.noreply.github.com>
2026-09-15 13:49:41 +0200

    sombrero

--- BalaJuego/Assets/Scripts/Guns/CharacterHat.cs
+++ BalaJuego/Assets/Scripts/Guns/CharacterHat.cs
@@ -5,16 +5,27 @@ public class CharacterHat : MonoBehaviour
     [SerializeField] int currentLives;
     [SerializeField] int startingLives;
     [SerializeField] Animator animator;
+    [SerializeField] ParticleSystem hatOffParticles;
 
     private void Start()
     {
     }
     //Returns true if no shield
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            setLives(Mathf.Abs(currentLives - 1));
+            hatOffParticles.Play();
+        }
+    }
     public bool getHit()
     {
         currentLives--;
         if (currentLives < 0)
         {
+            hatOffParticles.Play();
             return true;
         }
         if (animator)
